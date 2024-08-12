a62b8253ea7a2368691a4b7f75df33bc17d149d3
Prabhat kumar <prabhatsah349@gmail.com>
2024-08-12 13:18:48 +0530

    Second txt file added

--- Node js/app.js
+++ Node js/app.js
@@ -1,4 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
 
 const first = readFileSync("./content/first.txt", "utf8");
-console.log(first);
+const second = readFileSync("./content/second.txt", "utf8");
+console.log(first, second);
