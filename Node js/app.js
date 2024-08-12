5d036cea09b0b787bbde1538de602ec280a7200e
Prabhat kumar <prabhatsah349@gmail.com>
2024-08-12 17:45:25 +0530

    Commited on 12 Aug 17:45

--- Node js/app.js
+++ Node js/app.js
@@ -1,5 +1,40 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+//const util = require("util");
+//const readFilePromise = util.promisify(readFile);
+//const writeFilePromise = util.promisify(writeFile);
 
-const first = readFileSync("./content/first.txt", "utf8");
-const second = readFileSync("./content/second.txt", "utf8");
-console.log(first, second);
+const start = async () => {
+  try {
+    // const first = await readFilePromise("./content/first.txt", "utf8");
+    // const second = await readFilePromise("./content/second.txt", "utf8");
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt/",
+      `This is awesome : ${first} ${second}`,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+// getText("./content/first.txt")
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.log(err));
